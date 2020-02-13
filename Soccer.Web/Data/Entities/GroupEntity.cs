fe41c58df16b82eb6db34816edad8f60598506ff
Jimmy Davila <davila56jd@gmail.com>
2020-02-13 11:18:28 -0500

    added datatable

--- Soccer.Web/Data/Entities/GroupEntity.cs
+++ Soccer.Web/Data/Entities/GroupEntity.cs
@@ -6,16 +6,15 @@ using System.Threading.Tasks;
 
 namespace Soccer.Web.Data.Entities
 {
-    public class TeamEntity
+    public class GroupEntity
     {
         public int Id { get; set; }
 
-        [MaxLength(50, ErrorMessage = "The field {0} can not have more than {1} characters.")]
+        [MaxLength(30, ErrorMessage = "The field {0} can not have more than {1} characters.")]
         [Required(ErrorMessage = "The field {0} is required.")]
         public string Name { get; set; }
 
-        [Display(Name = "Logo")]
-        public string LogoPath { get; set; }
+        public TournamentEntity Tournament { get; set; }
 
     }
 }
